e42b554c576701c7af4e2ade86c119488102ac01
hadilo <devhadi@gmail.com>
2020-02-17 13:01:04 +0700

    n

--- deploy.py
+++ deploy.py
@@ -162,8 +162,8 @@ def send_email(zapier_hook, zapier_auth_prefix, zapier_auth, to, subject, body, 
     SENDGRID_EMAIL_DATA['personalizations'][0]['to'][0]['email'] = to
     SENDGRID_EMAIL_DATA['subject'] = subject
     SENDGRID_EMAIL_DATA['content'][0]['value'] = body
-    SENDGRID_EMAIL_DATA['attachments'][0]['content'] = image_64_encode
-    SENDGRID_EMAIL_DATA['attachments'][0]['filename'] = "aaa.apk"
+    # SENDGRID_EMAIL_DATA['attachments'][0]['content'] = image_64_encode
+    # SENDGRID_EMAIL_DATA['attachments'][0]['filename'] = "aaa.apk"
 
     auth = zapier_auth_prefix + " " + zapier_auth
     headers = {'Authorization': auth, 'Content-Type': 'application/json'}
