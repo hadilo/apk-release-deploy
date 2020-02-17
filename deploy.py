9d95eee36cc837abb80aeba844772a386cd186cf
hadilo <devhadi@gmail.com>
2020-02-17 11:11:00 +0700

    1

--- deploy.py
+++ deploy.py
@@ -145,9 +145,10 @@ def send_email(zapier_hook, zapier_auth_prefix, zapier_auth, to, subject, body, 
         bool: Send success/fail.
     '''
 
-    print("attach1 " + source_file)
-    print("\nattach2 " + open(source_file, 'rb'))
-    print("\nattach2 " + open(source_file, 'rb').encode("utf-8"))
+    source_file1 = "/" + source_file
+    print("attach1 " + source_file1)
+    # print("\nattach2 " + open(source_file1, 'rb'))
+    print("\nattach2 " + open(source_file1, 'rb').encode("utf-8"))
 
 
     # Get the Byte-Version of the image
