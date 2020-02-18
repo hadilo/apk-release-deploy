3a42b08c9eeff0c9ad06ebc54db733c517ceefc5
hadilo <devhadi@gmail.com>
2020-02-18 19:50:01 +0700

    berhasil pake service_account

--- gdrive.py
+++ gdrive.py
@@ -1,46 +1,66 @@
 from __future__ import print_function
+
+import io
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import argparse
+from apiclient import errors
 
 # If modifying these scopes, delete the file token.pickle.
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+def callback(request_id, response, exception):
+    if exception:
+        print(exception)
+    else:
+        print("Permission Id: %s" % response.get('id'))
 
-options = None
+def shareFile(drive_service, file_id, email):
+    batch = drive_service.new_batch_http_request(callback=callback)
+    user_permission = {
+        'type': 'user',
+        'role': 'writer',
+        'emailAddress': email
+    }
+    batch.add(drive_service.permissions().create(
+            fileId=file_id,
+            body=user_permission,
+            fields='id',
+    ))
+    batch.execute()
 
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            # flow = InstalledAppFlow.from_client_secrets_file('client_secrets.json', SCOPES) #client_secrets_file
-            flow = InstalledAppFlow.from_client_secrets_file(options.client_secrets_file, SCOPES)  # client_secrets_file
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+def delete_file(drive_service, file_id):
+  """Permanently delete a file, skipping the trash.
+  Args:
+    service: Drive API service instance.
+    file_id: ID of the file to delete.
+  """
+  try:
+    drive_service.files().delete(fileId=file_id).execute()
+  except errors.HttpError as error:
+    print('An error occurred: %s' % error)
 
-    service = build('drive', 'v3', credentials=creds)
+def download(drive_service, file_id):
+    request = drive_service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
 
-    # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+def upload(drive_service, name, file):
+    file_metadata = {'name': name}
+    media = MediaFileUpload(file)
+    file = drive_service.files().create(body=file_metadata,media_body=media,fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+
+# Call the Drive v3 API
+def getListAll(drive_service):
+    results = drive_service.files().list(pageSize=10, fields="files(id, name, webContentLink, webViewLink)").execute()
     items = results.get('files', [])
 
     if not items:
@@ -48,22 +68,23 @@ def main():
     else:
         print('Files:')
         for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+            print(item)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--release.dir', dest='release_dir', help='path to release folder', required=True)
-    parser.add_argument('--app.name', dest='app_name', help='app name that will be used as file name', required=True)
-    parser.add_argument('--changelog.file', dest='changelog_file', help='path to changelog file', required=True)
-    parser.add_argument('--template.file', dest='template_file', help='path to email template file', required=True)
-    # parser.add_argument('--dropbox.token', dest='dropbox_token', help='dropbox access token', required=True)
-    # parser.add_argument('--dropbox.folder', dest='dropbox_folder', help='dropbox target folder', required=True)
-    parser.add_argument('--zapier.hook', dest='zapier_hook', help='zapier email web hook', required=True)
-    parser.add_argument('--zapier.authprefix', dest='zapier_auth_prefix', help='zapier email web hook prefix',required=True)
-    parser.add_argument('--zapier.auth', dest='zapier_auth', help='zapier email web hook key', required=True)
-    parser.add_argument('--email.to', dest='email_to', help='email recipients', required=True)
-    parser.add_argument('--client_secrets.file', dest='client_secrets_file', help='path to release folder', required=True)
+    from google.oauth2 import service_account
 
-    options = parser.parse_args()
-    main()
+    # SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    SERVICE_ACCOUNT_FILE = 'service.json'
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # delegated_credentials = credentials.with_subject('devhadi@gmail.com')
+    drive_service = build('drive', 'v3', credentials=credentials)
+
+    getListAll(drive_service)
+
+    # download(drive_service, "1HwwsnULoutorJWR3LVf7eZVgNWkKkXyC")
+
+    # delete_file(drive_service, "1HwwsnULoutorJWR3LVf7eZVgNWkKkXyC")
+
